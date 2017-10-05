c5f24b86d62c404a6887686d8d3c899b818daed2
egiovann <elodie.giovannoli@gmail.com>
2017-10-05 14:07:21 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 library(plyr)
 
 ## Set your working directory
-dir <-""
-## Set the location of the data
+setwd()
+## Set the location of the data "household_power_consumption.txt"
 data_path <-""
 
 ## Read the data
@@ -12,6 +12,6 @@ data <- read.table(data_path, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=
 data_sub <- subset(data,(Date == "1/2/2007" | Date =="2/2/2007"))
 
 ##Create the png for plot1
-png(paste(dir,"plot1.png",sep=""),width=480, height=480)
+png("plot1.png",width=480, height=480)
 hist(as.numeric(data_sub$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
