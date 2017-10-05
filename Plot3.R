53c786ed089c0b2b6aacf69b61fb681200315374
egiovann <elodie.giovannoli@gmail.com>
2017-10-05 15:53:01 +0200

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -19,6 +19,6 @@ png("plot3.png",width=480, height=480)
 plot(data_sub$Date_and_Time,as.numeric(data_sub$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering")
 lines(data_sub$Date_and_Time,as.numeric(data_sub$Sub_metering_2),type="l",col="red")
 lines(data_sub$Date_and_Time,as.numeric(data_sub$Sub_metering_3),type="l",col="blue")
-legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_1","Sub_metering_1"))
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_1","Sub_metering_1"),bty="n")
 dev.off()
 
